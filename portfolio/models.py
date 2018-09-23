7da0d2817f53b85586bb0ad7ebea3a42fcba61de
Craig <caadler@unomaha.edu>
2018-09-22 21:35:43 -0500

    Fix for no investment error

--- portfolio/models.py
+++ portfolio/models.py
@@ -72,22 +72,22 @@ class Stock(models.Model):
 
     def current_stock_price(self):
         symbol_f = str(self.symbol)
-        main_api = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='
-        api_key = '&interval=1min&apikey=JPMPSPT7R9L3LK45'
+        main_api = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='
+        api_key = '&apikey=JPMPSPT7R9L3LK45'
         url = main_api + symbol_f + api_key
         json_data = requests.get(url).json()
         mkt_dt = (json_data["Meta Data"]["3. Last Refreshed"])
-        open_price = float(json_data["Time Series (1min)"][mkt_dt]["1. open"])
+        open_price = float(json_data["Weekly Time Series"][mkt_dt]["1. open"])
         share_value = open_price
         return share_value
 
     def current_stock_value(self):
         symbol_f = str(self.symbol)
-        main_api = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='
-        api_key = '&interval=1min&apikey=JPMPSPT7R9L3LK45'
+        main_api = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='
+        api_key = '&apikey=JPMPSPT7R9L3LK45'
         url = main_api + symbol_f + api_key
         json_data = requests.get(url).json()
         mkt_dt = (json_data["Meta Data"]["3. Last Refreshed"])
-        open_price = float(json_data["Time Series (1min)"][mkt_dt]["1. open"])
+        open_price = float(json_data["Weekly Time Series"][mkt_dt]["1. open"])
         share_value = open_price
         return float(share_value) * float(self.shares)
